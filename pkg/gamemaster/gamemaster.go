7cc8c95fa9e250faa08c42857b873a930b8d5c3b
robosyunn <robosyunn0319@gmail.com>
2020-05-15 22:10:12 +0900

    CreateMatchをかきましたたたたた
    Co-authored-by: さなけ <21271711+satackey@users.noreply.github.com>

--- pkg/gamemaster/gamemaster.go
+++ pkg/gamemaster/gamemaster.go
@@ -1,23 +1,54 @@
 package gamemaster
 
 import (
+	"errors"
+
 	"github.com/satackey/procon31server/pkg/apispec"
 )
 
-type GameMaster struct {
+type Match struct {
 	FieldStatus    apispec.FieldStatus
-	turnMillis     int
-	intervalMillis int
+	TurnMillis     int
+	IntervalMillis int
 }
 
-func (g *GameMaster) createMatch(FieldStatus, turnMillis, intervalMillis) {
-
+type GameMaster struct {
+	Matches map[int]*Match
 }
 
-func (g *GameMaster) getFieldByID(matchID) {
+func (g *GameMaster) CreateMatch(fieldStatus, turnMillis, intervalMillis) {
+	matchID := len(g.Matches)
+	// マップの数(=Matchの数)をmatchIDに
 
+	match := &Match{
+		FieldStatus:    fieldStatus,
+		TurnMillis:     turnMillis,
+		IntervalMillis: intervalMillis,
+		// 型: 値,
+	}
+	// FieldStatus, 各ターンの時間, ターン数をGameMasterで保管
+
+	g.Matches[matchID] = match
+	// matchIDをkeyにしてmap(Matches)に値(match)をセット
+
+	// todo 各ターン終了の時間に点数計算をするようにする
+
+	return macthID
+	// matchIDを関数の戻り値にする
 }
 
-func (g *GameMaster) postAgentActions(teamID, FieldStatusAction) {
+func (g *GameMaster) GetFieldByID(matchID) (apispec.FieldStatus, error) {
+	match, ok := g.Matches[matchID]
+
+	if !ok {
+		return nil, errors.New("エラーを表す文字列")
+	}
+	// 受け取ったmatchIDが存在するかの判定、存在しない場合はその旨をエラーで表す
+
+	return match.fieldStatus
+	// match(g.Matches[key])のなかのFieldStatus
+}
+
+func (g *GameMaster) PostAgentActions(teamID, FieldStatusAction) {
 
 }
