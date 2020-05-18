4bec7d0b066313f35f7570b84f6127c3ef275549
mmnk-github <48mimineko@gmail.com>
2020-05-18 23:04:45 +0900

    field_test.goめっちゃ追記

--- pkg/field/field_test.go
+++ pkg/field/field_test.go
@@ -2,12 +2,93 @@ package field
 
 import (
 	"testing"
+	"github.com/satackey/procon31server/pkg/apispec"
 )
 
-func (f *Field) TestCelSelectedTimesCount(t *testing.T) {
-	result := f.CellSelectedTimesCount(hoge)
-	expext := fuga
-	if result != expext {
+func TestCelSelectedTimesCount(t *testing.T) {
+	f := New()
+
+	points := [][]int{}
+	cells := [][]apispec.Cell{}
+	teams := []apispec.Team{}
+	actions := []apispec.FieldStatusAction{}
+
+	fieldStatus := &apispec.FieldStatus{
+		Width: 4,
+		Height: 3,
+		Points: points,
+		StartedAtUnixtime: 1576800000,
+		Turn: 0,
+		Cells: cells,
+		Teams: teams,
+		Actions: actions,
+	}
+
+	f.InitField(fieldStatus)
+	
+	isValid := []bool{true, true, true, true, true, true, true}
+	updateActions := []*apispec.UpdateAction{
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		&apispec.UpdateAction{
+			AgentID: 330,
+			DX: 1,
+			DY: 1,
+			Type: "move",
+			X: 0,
+			Y: 0,
+		},
+		
+	}
+	result := f.CellSelectedTimesCount(isValid, updateActions)
+	expext := [][]int{}
+	if result == expext {
 		// error‚ð‹L˜^
 		// t.Error("\nresult: ", result, "\nexpext: ", expext)
 	}
