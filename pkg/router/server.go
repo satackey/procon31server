f6d84ab5ce623754bcba3fbe0ebbbdf31e612d96
asann3 <e.4610ke@gmail.com>
2020-05-18 23:20:20 +0900

    進捗 5/18まで

--- pkg/router/server.go
+++ pkg/router/server.go
@@ -1,25 +1,52 @@
+// chromeとかで開いたページはGETとして認識される
+
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/ant0ine/go-json-rest/rest"
 	"log"
 	"net/http"
-	"time"
+    // "time"
+	// "../apisec/apisec.go"
+	"sync"
 )
 
 func main() {
+
+	// users := Users{
+	// 	Store: map[string]*User{},
+	// }
+
 	log.Println("localhost:3000 でサーバを起動しました")
 	api := rest.NewApi()
-	api.Use(&rest.AccessLogApacheMiddleware{})
-	api.Use(rest.DefaultCommonStack...)
+
+	// https://github.com/ant0ine/go-json-rest#streamingに使われていたapi
+	// api.Use(&rest.AccessLogApacheMiddleware{})
+	// api.Use(rest.DefaultCommonStack...)
+
+	// 基本的なやつ?
+	api.Use(rest.DefaultDevStack...)
 	router, err := rest.MakeRouter(
 		rest.Get("/stream", StreamThings),
-		rest.Get("/matches", MatchesThings),
+		// rest.Get("/matches", MatchesThings),
 		rest.Get("/matches/:id", FieldStatus),
+		// rest.Get("/matches/:id", users.FieldStatus),
 		// rest.Post("/matches/:id/action", Action),s
 		rest.Get("/ping", PingsThings),
 
+
+		// rest.Get("/matches", GetAllMatch),s
+
+		// 実際には降ってくるけど, 練習環境ではそれがないのでpostを用意しておく
+		// rest.Post("mathces", PostMatch),
+		// PathExp must start with / 
+		rest.Post("/matches", PostMatch),
+
+		// CRUDのC Post or Put
+		// rest.Get("/countries/:code", GetCountry),
+		// 必要かまだわからない
+		// rest.Delete("/countries/:code", DeleteCountry),
 
 	)
 	if err != nil {
@@ -36,20 +63,22 @@ type Thing struct {
 
 // StreamThings は
 func StreamThings(w rest.ResponseWriter, r *rest.Request) {
-	cpt := 0
-	for {
-		cpt++
-		w.WriteJson(
-			&Thing{
-				Name: fmt.Sprintf("thing #%d", cpt),
-			},
-		)
+	// cpt := 0
+	// for {
+	// 	cpt++
+
+		// w.WriteJson(
+		// 	&Thing{
+		// 		Name: fmt.Sprintf("thing #%d", cpt),
+		// 	},
+		// )
 		w.(http.ResponseWriter).Write([]byte("\n"))
 		// Flush the buffer to client
 		w.(http.Flusher).Flush()
 		// wait 3 seconds
-		time.Sleep(time.Duration(3) * time.Second)
-	}
+
+		// time.Sleep(time.Duration(3) * time.Second)
+	// }
 }
 
 // Ping は
@@ -59,30 +88,27 @@ type Ping struct {
 
 // PingsThings は
 func PingsThings(w rest.ResponseWriter, r *rest.Request) {
-	// for {
-	// 	cpt++
-	// 	w.WriteJson(
-	// 		&Thing{
-	// 			Name: fmt.Sprintf("thing #%d", cpt),
-	// 		},
-	// 	)
 
-	// ping := Ping{
-	// 	status: "OK",
-	// }
-	// w.WriteJson(&ping)
 	w.(http.ResponseWriter).Write([]byte("\n"))
 		// Flush the buffer to client
 	w.(http.Flusher).Flush()
-		// wait 3 seconds
-	time.Sleep(time.Duration(3) * time.Second)
-	// }
+
 }
 
 
 // Match は
 // type Match struct {
 // 	 string
+// }
+
+// type User struct {
+// 	Id string
+// 	Name string
+// }
+
+// type Users struct {
+// 	sync.RWMutex
+// 	Store map[string]*User
 // }
 
 // MatchesThings は
@@ -92,17 +118,39 @@ func MatchesThings(w rest.ResponseWriter, r *rest.Request) {
 		// Flush the buffer to client
 	w.(http.Flusher).Flush()
 		// wait 3 seconds
-	time.Sleep(time.Duration(3) * time.Second)
+	// time.Sleep(time.Duration(3) * time.Second)
 }
 
+// こういう構造体をapisec.goから引っ張ってきたい
+// type Status struct {
+//     Id int `json:id`
+// }
+
+
 // FieldStatus は
+// func (u *Users) FieldStatus(w rest.ResponseWriter, r *rest.Request) {
 func FieldStatus(w rest.ResponseWriter, r *rest.Request) {
+
+	// id := r.PathParam("id")
+	// fmt.Print(id)
+	// u.RLock()  // グローバル変数にアクセスするのに必要?
+	// var user *User
+	// if u.Store[id] != nil {
+	// 	user = &User{}
+	// 	*user = *u.Store[id]
+	// }
+	// u.RUnlock()
+	// if user == nil {
+	// 	rest.NotFound(w, r)
+	// 	return
+	// }
+	// w.WriteJson(user)
 
 	w.(http.ResponseWriter).Write([]byte("\n"))
 		// Flush the buffer to client
 	w.(http.Flusher).Flush()
 		// wait 3 seconds
-	time.Sleep(time.Duration(3) * time.Second)
+	// time.Sleep(time.Duration(3) * time.Second)
 }
 
 
@@ -112,4 +160,59 @@ func FieldStatus(w rest.ResponseWriter, r *rest.Request) {
 // 	w.(http.ResponseWriter).Write([]byte("\n"))
 // 		// Flush the buffer to client
 // 	w.(http.Flusher).Flush()
+// }
+
+
+// type Country struct {
+// 	Code string
+// 	Name string
+// }
+
+type Country struct {
+	Code int `json:"code"`
+	Name string `json:"name"`
+}
+
+
+// var store = map[string]*Country{}
+// var store = []*Country{}
+
+var lock = sync.RWMutex{}
+
+func PostMatch(w rest.ResponseWriter, r *rest.Request) {
+	// country := new(Country)
+	country := Country{}
+	// fmt.Print(country) { }が入ってた 初期化されている
+
+	// リクエストを読み取り, json.UnmarshalでJSONをデコードしてる
+	// &countryはJSONが入っている
+	err := r.DecodeJsonPayload(&country)
+	if err != nil {
+		rest.Error(w, err.Error(), http.StatusInternalServerError) // == 500
+		return
+	}
+	if country.Code == 0 {
+		rest.Error(w, "country code required", 400)
+		return
+	}
+	if country.Name == "" {
+		rest.Error(w, "country name required", 400)
+		return
+	}
+	lock.Lock()
+	// store[country.Code] = &country
+	lock.Unlock()
+	w.WriteJson(&country)
+}
+
+// func GetAllMatch(w rest.ResponseWriter, r *rest.Request) {
+// 	lock.RLock()
+// 	countries := make([]Country, len(store)) // mapを初期化してる storeの数だけ場所を確保している
+// 	i := 0
+// 	for _, country := range store {
+// 		countries[i] = *country
+// 		i++
+// 	}
+// 	lock.RUnlock()
+// 	w.WriteJson(&countries)
 // }
