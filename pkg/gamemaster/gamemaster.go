c7b5f788ddc101f214de097728a570449fa4e2e1
robosyunn <robosyunn0319@gmail.com>
2020-05-21 14:00:54 +0000

    StartAutoTurnUpdateをかきました
    Co-authored-by: さなけ <21271711+satackey@users.noreply.github.com>

--- pkg/gamemaster/gamemaster.go
+++ pkg/gamemaster/gamemaster.go
@@ -49,23 +49,14 @@ func (g *GameMaster) CreateMatch(fieldStatus *apispec.FieldStatus, startsAt int,
 	g.Matches[matchID] = match
 	// matchIDをkeyにしてmap(Matches)に値(match)をセット
 
-	match.Turnendcalc(1)
-	// 時間を計算する関数を呼び出す
-
-	go func(){
-		time.Sleep(time.Duration() * time.Millisecond)
-		// endtime秒後にfield.ActAgents()をしたい
-		// こんなかんじ？ぼんやりです
-		// 並列処理をしたい
-	}
-	// 各ターン終了の時間に点数計算をするようにする
+	match.StartAutoTurnUpdate()
 
 	return matchID, nil
 	// matchIDを関数の戻り値にする
 }
 
-// Turnendcalc は nターン終了時までの時間を計算する関数
-func (m *Match) Turnendcalc(n int) int {
+// GetRemainingMSecToTheTransitionOnTurn は nターン終了時までの時間を計算する関数
+func (m *Match) GetRemainingMSecToTheTransitionOnTurn(n int) int {
 	now := time.Now()
 	nowMillis := now.UnixNano() / int64(time.Millisecond)
 	startsAtMillis := int64(m.StartsAt) * 1000
@@ -75,6 +66,18 @@ func (m *Match) Turnendcalc(n int) int {
 	endtime := (startsAtMillis + int64(m.TurnMillis)*n64 + int64(m.IntervalMillis)*(n64-1)) - nowMillis
 	// 求めたいもの = (m.StartsAt /* s */ + m.TurnMillis /* ms */) - いま /* s */
 	return int(endtime)
+}
+
+// StartAutoTurnUpdate は 各ターン終了の時間に点数計算をする
+func (m *Match) StartAutoTurnUpdate() {
+
+	go func() {
+		time.Sleep(time.Duration(m.GetRemainingMSecToTheTransitionOnTurn(1)) * time.Millisecond)
+		// 時間を計算する関数を呼び出す
+		// endtime秒後にfield.ActAgents()をしたい
+		field.ActAgents()
+		// 渡す中身は後で、、、
+	}()
 }
 
 // GetFieldByID は 指定された試合IDの保管しているFieldStatusを返します
@@ -90,7 +93,7 @@ func (g *GameMaster) GetFieldByID(matchID int) (*apispec.FieldStatus, error) {
 	// match(g.Matches[key])のなかのFieldStatus
 }
 
-// PostAgentActions は
-func (g *GameMaster) PostAgentActions(teamID int, FieldStatusAction int) {
+// PostAgentActions は 各チームのエージェントの行動情報を受け取ります
+func (g *GameMaster) PostAgentActions(teamID int, FieldStatusAction *apispec.FieldStatusAction) {
 
 }
