7a391994f8e4277ddbcbae51884c87bd9107a2e8
mmnk-github <48mimineko@gmail.com>
2020-06-19 21:42:23 +0900

    いらない関数といらない記述を削除

--- pkg/field/field.go
+++ pkg/field/field.go
@@ -101,100 +101,10 @@ func (f *Field) CalcWallPoint(teamID int) int {
 	return sum
 }
 
-/*
-//stackを使うなら再帰関数は使わないほうが安全…
-func (f Field) dfs(vx int, vy int) {
-	var seen [f.Width][f.Height]bool{{}}
-	seen[vx][vy] = true
-
-	for {
-		if (seen[nextVx][nextVy]) {
-			continue
-		}
-
-	}
-}
-*/
-
-// SearchTiledBy は指定された座標の tiledby を返します
-// ある座標がわかっているときにそのtiledbyがわかる関数が欲しい！！
-func (f *Field) SearchTiledBy(x, y int) int {
-	var res int
-	res = f.Cells[y][x].TiledBy
-	// いい感じの処理、一行で書けちゃった（笑）
-	return res
-}
-
 // CalcAreaPoint は 現在のフィールドの指定されたチームIDの城壁の点数を計算します
 func (f *Field) CalcAreaPoint(teamID int) int {
 	Sum := 0
 
-	seen := [][]bool{{}}
-	todo := stack.New()
-
-	//右、下、左、上
-	dx := [4]int{1, 0, -1, 0}
-	dy := [4]int{0, 1, 0, -1}
-
-	for y, fieldRow := range f.Cells {
-		for x, cell := range fieldRow {
-			IsAreaPoint := true // 今見ている連結成分がエリアポイントかどうか。falseになった時点でどこかのセルが外側のセルである
-			SumKari := 0        // 今見ているセルを含む連結成分のPointの合計値を保存する変数
-
-			if cell.TiledBy == teamID {
-				//タイルが自陣か否か
-			} else {
-				if seen[y][x] {
-					// ここに入ったらDFSはしない、みたことある
-					continue
-				}
-				// ここに入ったらDFSをする、みたことない
-				todo.Push([2]int{x, y})
-
-				for todo.Len() != 0 { // スタックの中身があるならDFSを続ける　0にならない限り
-					v := todo.Pop().([2]int)
-					SumKari += f.Cells[v[0]][v[1]].Point
-
-					//外か内かの判定
-					if v[0] == 0 || v[0] == f.Width-1 || v[1] == 0 || v[1] == f.Height-1 {
-						IsAreaPoint = false
-					}
-
-					for index := 0; index < 4; index++ {
-						// x+dxが範囲外かどうか(範囲外ならcontinue), yについても同様
-						if x+dx[index] < 0 || x+dx[index] >= f.Width {
-							continue
-						}
-						if y+dy[index] < 0 || y+dy[index] >= f.Height {
-							continue
-						}
-
-						// 移動先が相手のタイルならcontinue
-						if f.SearchTiledBy(x+dx[index], y+dy[index]) != teamID || f.SearchTiledBy(x+dx[index], y+dy[index]) != -1 {
-							//移動先.TiledBy は 敵のIDになる
-							continue
-						}
-
-						// seen[x+dx][y+dy] が true なら既にみているからcontinue
-						if seen[x+dx[index]][y+dy[index]] {
-							continue
-						}
-
-						// seen[x+dx][y+dy] に true を代入
-						seen[x+dx[index]][y+dy[index]] = true
-
-						//v[0]:x  v[1]:y todoの配列
-						todo.Push([2]int{v[0] + dx[index], v[1] + dy[index]})
-					}
-				}
-			}
-			// SumKari の値が確定！！
-			if IsAreaPoint {
-				Sum += SumKari
-			}
-
-		}
-	}
 	return Sum
 }
 
