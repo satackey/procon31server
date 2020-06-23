dd7a8d9473a297a60bc3bea53214552f16a31b6c
asann3 <e.4610ke@gmail.com>
2020-06-23 22:05:17 +0900

    POSTのエラーを細かく書いた apispecの構造体参照をポインタに訂正した

--- pkg/router/server.go
+++ pkg/router/server.go
@@ -8,12 +8,11 @@ import (
 
 	"github.com/ant0ine/go-json-rest/rest"
 	"github.com/satackey/procon31server/pkg/apispec"
-
-	"sync"
 )
 
 func main() {
 
+	// apispec.Match.ID = 12
 	log.Println("localhost:3000 でサーバを起動しました")
 	api := rest.NewApi()
 
@@ -44,8 +43,6 @@ func PingsThings(w rest.ResponseWriter, r *rest.Request) {
 
 }
 
-var lock = sync.RWMutex{}
-
 // https://github.com/ant0ine/go-json-rest/blob/ebb33769ae013bd5f518a8bac348c310dea768b8/rest/response.go
 
 type ResponseWriter interface {
@@ -70,10 +67,10 @@ func MyResponse(w ResponseWriter, error string, code int) {
 }
 
 var StartAtUnixTime = "startAtUnixTime"
-var googlemap = map[int]*TwoResponse{}
+var TwoError = map[int]*TwoResponse{}
 
 func DoubleResponse(w ResponseWriter, error string, unixtime int, code int) {
-	// googlemap := TwoResponse{}
+	// TwoError := TwoResponse{}
 	w.WriteHeader(code)
 	// err := w.WriteJson(map[string]string{ErrorFieldName: error, StartAtUnixTime: unixtime})
 	err := w.WriteJson(TwoResponse{Status: error, UnixTime: unixtime})
@@ -88,8 +85,7 @@ type TwoResponse struct {
 }
 
 func GetAllMatch(w rest.ResponseWriter, r *rest.Request) {
-	lock.RLock()
-	allmatch := apispec.Match{}
+	allmatch := &apispec.Match{}
 	allmatch.Turns = 9999
 
 	if allmatch.ID == 0 {
@@ -123,9 +119,12 @@ func GetAllMatch(w rest.ResponseWriter, r *rest.Request) {
 
 func PostAction(w rest.ResponseWriter, r *rest.Request) {
 
-	matchstruct := apispec.Match{}
-	matchstruct.Turns = 9999
-	err := r.DecodeJsonPayload(&matchstruct)
+	actions := &apispec.UpdateAction{}
+	// actions := &apispec.Update{}
+	// action.Turns = 9999
+	actions.DX = 9999
+	actions.DY = 9999
+	err := r.DecodeJsonPayload(&actions)
 	if err != nil {
 		rest.Error(w, err.Error(), http.StatusInternalServerError) // == 500
 		return
@@ -135,34 +134,46 @@ func PostAction(w rest.ResponseWriter, r *rest.Request) {
 		rest.Error(w, err.Error(), http.StatusInternalServerError) // == 500
 		return
 	}
-	if matchstruct.ID == 0 {
-		rest.Error(w, "id required", 400)
+
+	if actions.AgentID == 0 {
+		rest.Error(w, "agentid required", 400)
 		return
 	}
-	if matchstruct.IntervalMillis == 0 {
-		rest.Error(w, "intervalmills ないぞ", 400)
+	if actions.DX == 9999 {
+		rest.Error(w, "DX ないぞ", 400)
 		return
 	}
-	if matchstruct.MatchTo == "" {
-		rest.Error(w, "Matchto required", 400)
+	if actions.DX < -1 || actions.DX > 1 {
+		rest.Error(w, "DXは-1から1の範囲にしましょう", 400)
 		return
 	}
-	if matchstruct.TeamID == 0 {
-		rest.Error(w, "teamid required", 400)
+	if actions.DY == 9999 {
+		rest.Error(w, "DY ないぞ", 400)
 		return
 	}
-	if matchstruct.TurnMillis == 0 {
-		rest.Error(w, "turnmillis required", 400)
+	if actions.DY < -1 || actions.DY > 1 {
+		// rest.Error(w, "dy required", 400)
+		DoubleResponse(w, "invalid", 0, 400)
 		return
 	}
-	if matchstruct.Turns == 9999 {
-		rest.Error(w, "turns required", 400)
+	// こっちは必要ないかも 必要だった
+	if actions.Type == "" {
+		rest.Error(w, "type ないぞ", 400)
 		return
 	}
-	// lock.Lock()
-	// store[matchstruct.Id] = &matchstruct
-	// lock.Unlock()
-	// w.WriteJson(&matchstruct)
+	if actions.Type != "move" || actions.Type != "remove" || actions.Type != "stay" {
+		rest.Error(w, "typeはmoveかremoveかstayで入力しよう", 400)
+		return
+	}
+	// if actions.Turns == 9999 {
+	// 	rest.Error(w, "turns required", 400)
+	// 	return
+	// }
+	// else {
+
+	//辺を越えようとしてきた場合のエラー
+	// 4辺の値 width height を事前情報から引っ張ってくる
+
 	MyResponse(w, "OK", 202)
 	return
 }
@@ -173,7 +184,7 @@ var ActionStore = map[int]*apispec.FieldStatusAction{}
 func GetUpdateAction(w rest.ResponseWriter, r *rest.Request) {
 
 	// TODO ここちょっと怪しい(値自体を読み込めていない気がする)
-	NewAction := apispec.FieldStatus{}
+	NewAction := &apispec.FieldStatus{}
 	NewAction.StartedAtUnixtime = 9999
 	// NewAction.Turn
 
@@ -212,7 +223,7 @@ func GetUpdateAction(w rest.ResponseWriter, r *rest.Request) {
 
 	// Teamの中身の確認
 	// TODO ここもちょっと怪しい
-	TeamStatus := apispec.Team{}
+	TeamStatus := &apispec.Team{}
 
 	if TeamStatus.TeamID == 0 {
 		rest.Error(w, "id required", 400)
@@ -232,6 +243,50 @@ func GetUpdateAction(w rest.ResponseWriter, r *rest.Request) {
 		rest.Error(w, "id required", 400)
 		return
 	}
+
+	// Agent
+	// AgentStatus := &apispec.Agent{}
+
+	// if AgentStatus.AgentID == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if AgentStatus.X == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if AgentStatus.Y == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+
+	// // FieldStatusのAction
+	// FieldAction := &apispec.FieldStatusAction{}
+
+	// if FieldAction.AgentID == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if FieldAction.DX > 3 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if FieldAction.DY == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if FieldAction.Type == "" {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if FieldAction.Apply == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
+	// if FieldAction.Turn == 0 {
+	// 	rest.Error(w, "id required", 400)
+	// 	return
+	// }
 
 	MyResponse(w, "OK", 202)
 	return
